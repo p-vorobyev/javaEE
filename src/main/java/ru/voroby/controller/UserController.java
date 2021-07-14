fca1a706e375f5ad292e9908bd1b06cc04055508
Pavel Vorobev <p.vorobyev@inbox.ru>
2021-07-14 11:50:45 +0300

    json-p functionality

--- src/main/java/ru/voroby/controller/UserController.java
+++ src/main/java/ru/voroby/controller/UserController.java
@@ -4,6 +4,10 @@ import ru.voroby.entity.User;
 import ru.voroby.repository.UserStore;
 
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.stream.JsonCollectors;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -16,6 +20,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
@@ -29,15 +34,17 @@ public class UserController {
     UriInfo uriInfo;
 
     @GET
-    public List<User> getUsers() {
-        return userStore.getAllUsers();
+    public JsonArray getUsers() {
+        return userStore.getAllUsers().stream()
+                .map(this::buildUserJson)
+                .collect(JsonCollectors.toJsonArray());
     }
 
     @GET
     @Path("{id}")
     public Response getUser(@PathParam("id") Integer id) {
         return userStore.getUser(id)
-                .map(userRecord -> Response.ok().entity(userRecord).build())
+                .map(user -> Response.ok().entity(buildUserJson(user)).build())
                 .orElseGet(() -> Response.noContent().build());
     }
 
@@ -50,6 +57,13 @@ public class UserController {
                 .build(id);
 
         return Response.created(uri).build();
+    }
+
+    private JsonObject buildUserJson(User user) {
+        return Json.createObjectBuilder()
+                .add("id", user.getId())
+                .add("name", user.getName())
+                .add("age", user.getAge()).build();
     }
 
 }
