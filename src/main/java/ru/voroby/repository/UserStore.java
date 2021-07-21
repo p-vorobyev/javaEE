edfa1cb216d24d6ea0719a4817461918cf41b164
Pavel Vorobev <p.vorobyev@inbox.ru>
2021-07-21 18:29:58 +0300

    transactional management

--- src/main/java/ru/voroby/repository/UserStore.java
+++ src/main/java/ru/voroby/repository/UserStore.java
@@ -4,22 +4,24 @@ import lombok.extern.slf4j.Slf4j;
 import ru.voroby.entity.User;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionManagement;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
 
 @Slf4j
-@Transactional
+@TransactionManagement
 @Stateless
 public class UserStore implements Serializable {
 
     @PersistenceContext(unitName = "EE")
     private EntityManager em;
 
+    @TransactionAttribute
     public int addUser(User user) {
         if (user.getId() != null) {
             em.merge(user);
@@ -32,11 +34,13 @@ public class UserStore implements Serializable {
         return user.getId();
     }
 
+    @TransactionAttribute
     public Optional<User> getUser(int id) {
         User user = em.find(User.class, id);
         return user != null ? Optional.of(user) : Optional.empty();
     }
 
+    @TransactionAttribute
     public List<User> getAllUsers() {
         return em.createNamedQuery(User.ALL, User.class).getResultList();
     }
